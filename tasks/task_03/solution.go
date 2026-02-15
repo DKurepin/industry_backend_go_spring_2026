9eed4489e4daa6ddb1029cdd2f6c24b4aff0183d
Daniil Kurepin <daniil.kurepin@gmail.com>
2026-02-16 00:01:40 +0300

    task_03: done

--- tasks/task_03/solution.go
+++ tasks/task_03/solution.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 func fizzBuzz(n int) (string, error) {
-	// TODO
+	if n < 1 {
+		return "", errors.New("number must be positive")
+	}
+	if n%3 == 0 && n%5 == 0 {
+		return "FizzBuzz", nil
+	}
+	if n%3 == 0 {
+		return "Fizz", nil
+	}
+	if n%5 == 0 {
+		return "Buzz", nil
+	}
+	return fmt.Sprintf("%d", n), nil
 }
