330ed88de497ef63e4c7a4bf0e06279da7930d2a
Daniil Kurepin <daniil.kurepin@gmail.com>
2026-02-16 00:06:34 +0300

    task_04: done

--- tasks/task_04/solution.go
+++ tasks/task_04/solution.go
@@ -1,9 +1,35 @@
 package main
 
+import "math"
+
 type Stats struct {
-	// TODO: define the struct
+	Count int
+	Sum   int64
+	Min   int64
+	Max   int64
 }
 
-func Calc(nums []int64) Stats {
-	// TODO
+func Calc(xs []int64) Stats {
+	if len(xs) == 0 {
+		return Stats{}
+	}
+
+	stats := Stats{
+		Count: len(xs),
+		Sum:   0,
+		Min:   math.MaxInt64,
+		Max:   math.MinInt64,
+	}
+
+	for _, x := range xs {
+		stats.Sum += x
+		if x < stats.Min {
+			stats.Min = x
+		}
+		if x > stats.Max {
+			stats.Max = x
+		}
+	}
+
+	return stats
 }
