41bfe01a64b79c6b6a82f2cc8844eef862932626
Daniil Kurepin <daniil.kurepin@gmail.com>
2026-02-16 22:52:47 +0300

    task_06: done

--- tasks/task_06/solution.go
+++ tasks/task_06/solution.go
@@ -1,3 +1,61 @@
 package main
 
-// TODO
+import "container/list"
+
+type node[K comparable, V any] struct {
+	key   K
+	value V
+}
+
+type LRUCache[K comparable, V any] struct {
+	capacity int
+	list     *list.List
+	items    map[K]*list.Element
+}
+
+func NewLRUCache[K comparable, V any](capacity int) *LRUCache[K, V] {
+	return &LRUCache[K, V]{
+		capacity: capacity,
+		list:     list.New(),
+		items:    make(map[K]*list.Element),
+	}
+}
+
+func (c *LRUCache[K, V]) Get(key K) (V, bool) {
+	if c.capacity == 0 {
+		var zero V
+		return zero, false
+	}
+
+	element, ok := c.items[key]
+	if !ok {
+		var zero V
+		return zero, false
+	}
+
+	c.list.MoveToFront(element)
+
+	return element.Value.(node[K, V]).value, true
+}
+
+func (c *LRUCache[K, V]) Set(key K, value V) {
+	if c.capacity == 0 {
+		return
+	}
+
+	if element, ok := c.items[key]; ok {
+		element.Value = node[K, V]{key: key, value: value}
+		c.list.MoveToFront(element)
+		return
+	}
+
+	newNode := node[K, V]{key: key, value: value}
+	element := c.list.PushFront(newNode)
+	c.items[key] = element
+
+	if len(c.items) > c.capacity {
+		oldest := c.list.Back()
+		c.list.Remove(oldest)
+		delete(c.items, oldest.Value.(node[K, V]).key)
+	}
+}
